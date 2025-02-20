2a653955697eb512ba2cfa0b60938e32a4d4ee87
BuildTools <unconfigured@null.spigotmc.org>
2025-02-20 15:50:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using NetworkServices.Interface;
+
+namespace Port80_443Detector {
+    class Program {
+        static async Task Main(string[] args) {
+            if(args.Length != 1) return;
+
+            string ip = args[0];
+            if (await PortDetector.ExistsAsync(ip, 80, 500)) {
+                Console.WriteLine("porta 80 detectada");
+            } else if (await PortDetector.ExistsAsync(ip, 443, 500)) {
+                Console.WriteLine("porta 443 detectada");
+            } else {
+                Console.WriteLine("porta não detectada");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
